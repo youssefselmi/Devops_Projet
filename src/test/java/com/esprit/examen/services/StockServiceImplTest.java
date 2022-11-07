33e2d5c2b754abb492281246a06aaa44cee2cbd9
youssefselmi <youssef.selmi@esprit.tn>
2022-11-07 04:54:07 +0100

    test2

--- src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -8,8 +8,8 @@ import static org.junit.Assert.assertNull;
 import java.util.List;
 
 import org.junit.jupiter.api.Order;
-//import org.junit.jupiter.api.Test;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+//import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
